8a1a6f5f9609b7d47e16d00f993fa5f559a2b374
Wayne Rose <e.guishardrose@hotmail.co.uk>
2021-10-20 18:05:11 +0100

    Unittests.py

--- Unittests.py
+++ Unittests.py
@@ -1,19 +1,26 @@
 import unittest
 from ASOS_Webscraper import AsosScraper 
 from selenium import webdriver 
+import hypothesis
 
+# import unittest
+# from example.cart import ShoppingCart
+# from example.product import Product
 class ASOS_Webscraper_Tests(unittest.TestCase):
 
     def setUp(self):
-        self.AsosScraper = AsosScraper("https://www.asos.com/", "men")
+        self.AsosScraper = AsosScraper(webdriver.Chrome(),'men')
         
-    def go_to_website(self):
-        expected_value = "https://www.asos.com/men"
-        actual_value = driver.get(URL)
-        self.assertEqual(expected_value, actual_value)
+    def test_go_to_website(self):
+        # expected_value = "https://www.asos.com/men"    
+        self.assertTrue(webdriver.Chrome().get("https:/www.asos.com/men"))
+    # def number_of_extracted_urls(self):
+    #     expected_value = 72
+    #     actual_value = print(len(self.links))
+    #     self.assertEqual(expected_value,actual_value)
 
-    def click_on_accept_cookies_button(self):
-        expected_value = 
+    
+      
 
 
     def tearDown(self):
@@ -23,4 +30,8 @@ class ASOS_Webscraper_Tests(unittest.TestCase):
 
 unittest.main(argv=[''], verbosity=2, exit=False)
         
-        
+# Test that the functions goes to websites. 
+# In hypothesis, there's a module which tests URLs.
+# For accept cookies, could send a bunch of xpaths which share the element of the 
+# accept cookies button and see if it doesn't give an exception. 
+
