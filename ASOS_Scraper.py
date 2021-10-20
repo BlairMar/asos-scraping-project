f84fe4709e463e06ca03f4ef11dd2757a8bee6eb
Miruna <miruna_nitu@yahoo.com>
2021-10-20 09:55:04 +0300

    Add new method to reduce code

--- ASOS_Scraper.py
+++ ASOS_Scraper.py
@@ -4,7 +4,6 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 import itertools
-import urllib.request
 
 class AsosScraper:
      def __init__ (self, driver: webdriver.Chrome(), gender: str):
@@ -14,45 +13,32 @@ class AsosScraper:
         self.driver = driver 
         driver.get(URL)
         self.a = ActionChains(self.driver)  #object of ActionChains; it ads hover over functionality 
-
+        self.links = [] # Initialize links, so if the user calls for extract_links inside other methods, it doesn't throw an error
+       
+     def extract_links(self, xpath: str): # given a common xpath, this method extracts the unique xpaths in a list and get the 'href' attribute for every unique xpath of an webelement
+         xpaths_list = self.driver.find_elements_by_xpath(xpath) 
+         self.links = []
+         for item in xpaths_list:
+            self.links.append(item.find_element_by_xpath('.//a').get_attribute('href'))
+     
      def accept_cookies_button(self):
         time.sleep(4)
         click_accept_cookies = self.driver.find_element_by_xpath('//button[@class="g_k7vLm _2pw_U8N _2BVOQPV"]')
         click_accept_cookies.click()
      
-     def choose_category(self):     
-
+     def choose_category(self):  #this method finds the second button (out of 12) from the category buttons top bar, and perform a hover over element function 
         category_button = self.driver.find_element_by_xpath('//*[@id="chrome-sticky-header"]/div[2]/div[2]/nav/div/div/button[2]')
         self.a.move_to_element(category_button).perform()  
-        time.sleep(4)
+        time.sleep(4)            #this will hover over the "New in" category and show the 'New in' subcategories
 
-     def get_submenu_product_list(self):     #show submenu and get a list with xpaths for evry subcategory
-        self.submenu_xpath_product_list = self.driver.find_elements_by_xpath('//*[@id="029c47b3-2111-43e9-9138-0d00ecf0b3db"]/div/div[2]/ul/li[1]/ul/li[*]')
-        self.new_list = []  
-        for item in self.submenu_xpath_product_list: #create a new list witht the 'href's for every webelement in the above list 
-            link = item.find_element_by_xpath('.//a').get_attribute('href')
-            self.new_list.append(link)
-            
-     def choose_subcategory(self):
-        self.driver.get(self.new_list[0])  #goes to Men/New in/View All, where self.new_list contains the 'href's for 
-                                           #all the subcategories in Men/New in and self.new_list[0] = 'View all', the first element of the list
-            # for item in self.new_list:
-            #    self.driver.get(item)                                  
-
-     def get_product_list_urls(self):
-        product_container = self.driver.find_element_by_xpath('//*[@id="plp"]/div/div/div[2]/div/div[1]/section[1]') 
-        self.product_list = product_container.find_elements_by_xpath('./article')
-        self.product_urls = []
-        for item1 in self.product_list:
-            link1 = item1.find_element_by_xpath('.//a').get_attribute('href')
-            self.product_urls.append(link1)
-
-   #   def go_to_product(self):
-   #      n = 3 #n tells how many prodducts we want to look at
-   #      #need an empty dictionary for the details 
-   #      for product in itertools.islice(self.product_urls,n):
-   #          self.driver.get(product)
-         
+     def get_submenu_product_list(self): #this method uses the "extract_links" method to access the first href (self.links[0]) in the "New in" subcategories list, which is "New in -> View all "
+         self.extract_links('//*[@id="029c47b3-2111-43e9-9138-0d00ecf0b3db"]/div/div[2]/ul/li[1]/ul/li[*]')
+         self.driver.get(self.links[0])
+     
+     def get_product_list_urls(self): #this method uses the "extract_links" method to extract the hrefs for every product in the 'New in/View all' 
+        self.extract_links('//*[@id="plp"]/div/div/div[2]/div/div[1]/section[1]/article')
+        self.product_urls = self.links #save the list with hrefs in another variable that will be reffered to in the following methods
+    
      def product_information(self):
             ''' 
     #TODO: Add image scraping functionality and create folder to add them to. Naming files appropriately too.
@@ -91,8 +77,6 @@ class AsosScraper:
                except:
                    self.product_information_dict[f'Product{url_counter}'][key].append('No information found')
         
-               self.src = driver.find_element_by_xpath('//img[@class="_2UpQX"]').get_attribute('src')
-               
                if url_counter == 4: #breaks after 3 items just for testing purposes
                  break
         
@@ -104,6 +88,5 @@ product_search = AsosScraper(webdriver.Chrome(),'men')
 product_search.accept_cookies_button()
 product_search.choose_category()
 product_search.get_submenu_product_list()
-product_search.choose_subcategory()
 product_search.get_product_list_urls()
 product_search.product_information()
