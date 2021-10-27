9e088550b1b55a891dc3050f4d34aa0c4726f4fa
Miruna <miruna_nitu@yahoo.com>
2021-10-27 11:01:38 +0100

    MN - Add improvements

--- ASOS_Scraper.py
+++ ASOS_Scraper.py
@@ -67,7 +67,7 @@ class AsosScraper:
             self.xpath_dict = {
                 'Product Name': '//*[@id="aside-content"]/div[1]/h1',
                 'Price': '//*[@id="product-price"]/div/span[2]/span[4]/span[1]',
-                'Product Details' : '//*[@id="product-details-container"]/div[1]/div/ul/li[*]',
+                'Product Details' : '//*[@id="product-details-container"]/div[1]/div/ul/li',
                 'Product Code': '/html/body/div[2]/div/main/div[2]/section[2]/div/div/div/div[2]/div[1]/p',
                 'Colour': '//*[@id="product-colour"]/section/div/div/span'
                 }
@@ -75,7 +75,7 @@ class AsosScraper:
             for url in self.product_urls:  # TODO: use enumerate
                self.driver.get(url)
                url_counter += 1
-               if url_counter == 3: #breaks after 3 items just for testing purposes
+               if url_counter == 4: #breaks after 3 items just for testing purposes
                  break
 
                self.product_information_dict = {
@@ -87,31 +87,30 @@ class AsosScraper:
             'Colour': []
             }
             }
-               try:  # find details info
-                  for key in self.xpath_dict:
-                   dict_key = self.driver.find_element_by_xpath(
-                       self.xpath_dict[key])
-                   self.product_information_dict[f'Product{url_counter}'][key].append(dict_key.text)
+               try: #find details info
+                   for key in self.xpath_dict:
+                     if key == 'Product Details':
+                        details_container = self.driver.find_elements_by_xpath(self.xpath_dict[key])
+                        for detail in details_container:
+                            self.product_information_dict[f'Product{url_counter}'][key].append(detail.text)
+
+                     else:
+                        dict_key = self.driver.find_element_by_xpath(self.xpath_dict[key])
+                        self.product_information_dict[f'Product{url_counter}'][key].append(dict_key.text)
+
                except:
-                   self.product_information_dict[f'Product{url_counter}'][key].append('No information found')
-               
-               try: # find product details info 
-                    details_container = self.driver.find_elements_by_xpath(self.xpath_dict['Product Details'])
-                    for detail in details_container:
-                        self.product_information_dict[f'Product{url_counter}']['Product Details'].append(detail.text)
-               except:  
-                    self.product_information_dict[f'Product{url_counter}']['Product Details'].append('No information found')  
-
-               #download the each product images to the images folder        
-            #    self.xpath_src_list = self.driver.find_elements_by_xpath('//*[@id="product-gallery"]/div[1]/div[2]/div[*]/img')
-            #    self.src_list = []
-            #    for xpath_src in self.xpath_src_list:
-            #       self.src_list.append(xpath_src.get_attribute('src'))
+                  self.product_information_dict[f'Product{url_counter}'][key].append('No information found')
+                   
+            #download the each product images to the images folder        
+            self.xpath_src_list = self.driver.find_elements_by_xpath('//*[@id="product-gallery"]/div[1]/div[2]/div[*]/img')
+            self.src_list = []                                        
+            for xpath_src in self.xpath_src_list:
+               self.src_list.append(xpath_src.get_attribute('src'))
         
-            #    for i,src in enumerate(self.src_list):
-            #        urllib.request.urlretrieve(src, f"images\{self.gender}_Product{url_counter}.{i}.jpg")
+            for i,src in enumerate(self.src_list[:-1]):   
+               urllib.request.urlretrieve(src, f"images\{self.gender}_Product{url_counter}.{i}.jpg")
 
-            #    print(self.product_information_dict)
+            print(self.product_information_dict)
      
     
 
