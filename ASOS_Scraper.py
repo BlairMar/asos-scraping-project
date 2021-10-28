79fd2a489442842903cf12e4c435e45d8ad11a9e
James Moody <James-moody@outlook.com>
2021-10-28 14:33:40 +0100

    Added more functionality to save_to_json method

--- ASOS_Scraper.py
+++ ASOS_Scraper.py
@@ -4,6 +4,7 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 import itertools
+import json
 
 class AsosScraper:
      def __init__ (self, driver: webdriver.Chrome(), gender: str):
@@ -13,9 +14,10 @@ class AsosScraper:
         self.driver = driver 
         driver.get(URL)
         self.a = ActionChains(self.driver)  #object of ActionChains; it ads hover over functionality 
+        self.product_dict_list = {}
 
      def accept_cookies_button(self):
-        time.sleep(4)
+        time.sleep(1)
         click_accept_cookies = self.driver.find_element_by_xpath('//button[@class="g_k7vLm _2pw_U8N _2BVOQPV"]')
         click_accept_cookies.click()
      
@@ -23,7 +25,7 @@ class AsosScraper:
 
         category_button = self.driver.find_element_by_xpath('//*[@id="chrome-sticky-header"]/div[2]/div[2]/nav/div/div/button[2]')
         self.a.move_to_element(category_button).perform()  
-        time.sleep(4)
+        time.sleep(1)
 
      def get_submenu_product_list(self):     #show submenu and get a list with xpaths for evry subcategory
         self.submenu_xpath_product_list = self.driver.find_elements_by_xpath('//*[@id="029c47b3-2111-43e9-9138-0d00ecf0b3db"]/div/div[2]/ul/li[1]/ul/li[*]')
@@ -54,27 +56,25 @@ class AsosScraper:
          
      def product_information(self):
         ''' 
-    #TODO: Add image scraping functionality and create folder to add them to. Naming files appropriately too.
-    A function to iterate through the product URL's of a webpage,
-    scrape product data and then append the data to a dictionary.
-    Attributes:
-        xpath_dict : a dictionary containing details to be scraped and relevant xpath
-        product_information_dict : empty dictionary to have scraped details appended to
-    Returns:
-        Data organised into a dictionary for every product visited
-    '''
+         Attributes:
+         xpath_dict : a dictionary containing details to be scraped and relevant xpath
+         product_information_dict : empty dictionary to have scraped details appended to
+         Returns:
+         Data organised into a dictionary for every product visited
+        '''
+        
         self.xpath_dict = {
                 'Product Name' : '//*[@id="aside-content"]/div[1]/h1', 
                 'Price' : '//*[@id="product-price"]/div/span[2]/span[4]/span[1]', 
-                'Product Details' : list, 
+                # 'Product Details' : list, 
                 'Product Code' : '/html/body/div[2]/div/main/div[2]/section[2]/div/div/div/div[2]/div[1]/p', 
                 'Colour' : '//*[@id="product-colour"]/section/div/div/span' 
                 }
         url_counter = 0
         for url in self.product_urls: #TODO: use enumerate
-          self.driver.get(url)
-          url_counter += 1
-          product_information_dict = {
+               self.driver.get(url)
+               url_counter += 1
+               self.product_information_dict = {
                                     f'Product{url_counter}':{
             'Product Name' : [], 
             'Price' : [], 
@@ -83,25 +83,30 @@ class AsosScraper:
             'Colour' : []
             }
             }
-          try: #find details info
+               try: #find details info
                   for key in self.xpath_dict:
                    dict_key = self.driver.find_element_by_xpath(self.xpath_dict[key])
                    self.product_information_dict[f'Product{url_counter}'][key].append(dict_key.text)
-          except:
+               except:
                    self.product_information_dict[f'Product{url_counter}'][key].append('No information found')
         
-          if url_counter == 4: #breaks after 3 items just for testing purposes
-            break
-
-        print(product_information_dict)
-    
-      #   self.driver.navigate().back()
-     #def save_to_json(self):
-         # test_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
-         # with open('JSON_test.json', mode='w') as f:
-         # json.dump(test_dict, f)
+               if url_counter == 4: #breaks after 3 items just for testing purposes
+                 break
+               self.product_dict_list.update(self.product_information_dict)
+               #print(self.product_information_dict)
+        
+        print(self.product_dict_list)
+        return(self.product_dict_list)
+     
+     
+     
+     def save_to_json(self):
+       #with open('JSON-files\new-in\view-all.json', mode='a+') as f: 
+        with open('JSON_details.json', mode='a+') as f:
+           json.dump(self.product_dict_list, f, indent=4) #'indent = x' to format output in json file, visually better
+           f.write('\n')
+         #TODO:# Make function more generalised (i.e reusable)
          
-
 
 product_search = AsosScraper(webdriver.Chrome(),'men')
 product_search.accept_cookies_button()
@@ -110,3 +115,4 @@ product_search.get_submenu_product_list()
 product_search.choose_subcategory()
 product_search.get_product_list_urls()
 product_search.product_information()
+product_search.save_to_json()
